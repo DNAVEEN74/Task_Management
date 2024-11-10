9032464b93a7f7b7e881f6a7df6be02558dd771a
unknown <naveendurgam74@gmail.com>
2024-11-10 22:35:39 +0530

    updated

--- frontend/src/components/searchBar.tsx
+++ frontend/src/components/searchBar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { todoListAtom } from '../Atoms/tasksSelector';
 import TodoCard from './todosBoard/todoCard';
+import { Filter, ChevronDown, Search } from 'lucide-react';
 
 export default function SearchBar(): JSX.Element {
     const [searchTerm, setSearchTerm] = useState('');
@@ -22,6 +23,7 @@ export default function SearchBar(): JSX.Element {
             <div className="flex justify-between bg-[#ECEDEE] w-full h-20 content-center rounded-2xl px-5 py-5 shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
                 <div className="bg-white w-[308px] h-11 px-2.5 py-3 rounded-full shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
                     <div className='flex w-[284px] h-5 gap-2 items-center'>
+                    <Search className='w-5 h-5' />
                         <input
                             type="text"
                             placeholder='Search Project'
@@ -33,6 +35,11 @@ export default function SearchBar(): JSX.Element {
                             }}
                         />
                     </div>
+                </div>
+                <div className='w-[107px] h-[39px] rounded-[5px] border border-[#625F6D] bg-white flex justify-center items-center gap-2' >
+                    <Filter className='w-4 h-4' />
+                    <p className='w-[39px] h-[19px] font-medium leading-[19.36px] text-base color-[#797979] ' >Filter</p>
+                    <ChevronDown className='w-4 h-4' />
                 </div>
             </div>
 
